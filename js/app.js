dd7511d3f60e73b85112a551bb99a6a7cbbfa1b6
saba <mahdaneparsa@gmail.com>
2025-01-16 10:42:27 +0330

    add alert to project for success or error in adding task to todolist project

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-button");
+const alertMessage = document.getElementById("alert-message");
 
 const todos = [];
+
+//ورودی های تایع یکی برای وارد کردن متن پیام و یکی برای استایل دهی به رنگ پیام
+const showAlert = (message, type) => {
+    // هربار که یک پیام ایجاد میشود قبلی را پاک میکند
+    alertMessage.innerHTML = "";
+  const alert = document.createElement("p");
+  alert.innerText = message;
+  alert.classList.add("alert");
+  alert.classList.add(`alert-${type}`);
+  alertMessage.append(alert);
+
+  // بعد از 2 ثانیه پیام را پاک میکند
+  setTimeout(() => {
+    alert.style.display = "none";
+  } , 2000);
+};
 
 const addHandler = () => {
   const task = taskInput.value;
@@ -20,8 +37,10 @@ const addHandler = () => {
     taskInput.value = "";
     dateInput.value = "";
     console.log(todos);
-  }else{
-    alert("warnning")
+    showAlert("Todo added successfully", "success");
+  } else {
+    //استفاده از تایع بالا و وارد کردن مقادیر ورودی و تایپ پیام
+    showAlert("please enter a todo !", "error");
   }
 };
 
