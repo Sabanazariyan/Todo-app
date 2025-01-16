0df4cda9923e090d15307dcf864bab572a078ec9
saba <mahdaneparsa@gmail.com>
2025-01-16 21:48:15 +0330

    edit todos task and date

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const addButton = document.getElementById("add-button");
 const alertMessage = document.getElementById("alert-message");
 const todosBody = document.querySelector("tbody");
 const deleteAllButton = document.getElementById("delete-all-button");
+const editButton = document.getElementById("edit-button");
 
 // ارایه اصلی تودوز اطلاعات خود را از لوکال استوریج میگیرد
 //اگر برای بار اول اجرا میشد و لوکال استوریج خالی بود یا قرار میدهیم و یک ارایه خالی میگذاریم
@@ -49,7 +50,7 @@ const displayTodos = () => {
         <td>${todo.date || "No Date"}</td>
         <td>${todo.completed ? "Completed" : "Pending"}</td>
         <td>
-        <button>Edit</button>
+        <button onclick="editHandler('${todo.id}')">Edit</button>
         <button onclick="toggleHandler('${todo.id}')">
           ${todo.completed ? "Undo" : "Do"}
         </button>
@@ -137,6 +138,30 @@ const toggleHandler = (id) => {
   showAlert("Todo status changed successfully", "success");
 };
 
+const editHandler = (id) => {
+  const todo = todos.find((todo) => todo.id === id);
+  taskInput.value = todo.task;
+  dateInput.value = todo.date;
+  addButton.style.display = "none";
+  editButton.style.display = "inline-block";
+  editButton.dataset.id = id;
+};
+
+const applyEditHandler = (event) => {
+  const id = event.target.dataset.id;
+  const todo = todos.find((todo) => (todo.id = id));
+  todo.task = taskInput.value;
+  todo.date = dateInput.value;
+  taskInput.value = "";
+  dateInput.value = "";
+  addButton.style.display = "inline-block";
+  editButton.style.display = "none";
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo edited successfully", "success");
+};
+
 window.addEventListener("load", displayTodos());
 addButton.addEventListener("click", addHandler);
 deleteAllButton.addEventListener("click", deleteAllHandler);
+editButton.addEventListener("click", applyEditHandler);
